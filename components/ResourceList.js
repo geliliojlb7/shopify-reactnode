d8e4d2ace4cd7bf90385cf5bc1bba0771985e246
geliliojlb7 <geliliojohnlloydb7@gmail.com>
2020-03-18 17:13:17 +0800

    init commit

---
+++ components/ResourceList.js
@@ -0,0 +1,64 @@
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import {
+  Card,
+  ResourceList,
+  Stack,
+  TextStyle,
+  Thumbnail
+} from "@shopify/polaris";
+import store from "store-js";
+
+const GET_PRODUCTS_BY_ID = gql`
+  query getProducts($ids: [ID!]!) {
+    nodes(ids: $ids) {
+      ... on Product {
+        title
+        handle
+        descriptionHtml
+        id
+        images(first: 1) {
+          edges {
+            node {
+              originalSrc
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+class ResourceListWithProducts extends React.Component {
+  render() {
+    const twoWeeksFromNow = new Date(Date.now() + 12096e5).toDateString();
+
+    return (
+      <Query query={GET_PRODUCTS_BY_ID} variables={{ ids: store.get("ids") }}>
+        {({ data, loading, error }) => {
+          if (loading) return <div>Loading...</div>;
+
+          if (error) return <div>{error.message}</div>;
+
+          console.log(data);
+          return (
+            <Card>
+              <p>Stuff here</p>
+            </Card>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default ResourceListWithProducts;
---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import { Page, Layout, EmptyState } from "@shopify/polaris";
+import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
+import store from 'store-js';
+import ResourceListWithProducts from '../components/ResourceList'
+const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
+
+class Index extends React.Component {
+  state = {
+    open: false
+  }
+
+  handleSelection = (resources) => {
+    const idsFromResources = resources.selection.map((product) => product.id)
+    this.setState({ open: false })
+
+    store.set('ids', idsFromResources)
+  };
+
+  render() {
+    return (
+      <Page>
+        <TitleBar
+          primaryAction={{
+            content: "Select products",
+            onAction: () => this.setState({ open: true })
+          }}
+        />
+        <ResourcePicker 
+          resourceType="Product"
+          showVariants={false}
+          open={this.state.open}
+          onSelection={(resources) => this.handleSelection(resources)}
+          onCancel={() => this.setState({ open: false })}
+        />
+        <Layout>
+          <EmptyState
+            heading="Discount your products temporarily"
+            action={{
+              content: "Select products",
+              onAction: () => this.setState({ open: true })
+            }}
+            image={img}
+          >
+            <p>Select products to change their price temporarily.</p>
+          </EmptyState>
+        </Layout>
+        <ResourceListWithProducts />
+      </Page>
+    );
+  }
+}
+
+export default Index;
